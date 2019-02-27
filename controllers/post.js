fef32974f34c8d481896631e51b30458719a70c0
ChaseAlexanderRoss <chase.alexander.ross@gmail.com>
2019-02-27 09:24:02 -0500

    post controller built (NT)

---
+++ controllers/post.js
@@ -0,0 +1,56 @@
+const { Post } = require('../models/post')
+
+module.exports = {
+    new: function(req, res) {
+        res.render("post/new");
+      },
+      create: function(req, res) {
+        const { title, description, link } = req.body;
+        Post.create({
+          title,
+          description,
+          link,
+        }).then(post => {
+          res.redirect(`/post/${post._id}`);
+        });
+      },
+      show: function(req, res) {
+        Post.findById(req.params.id).then(post => {
+          res.render("post/show", { post });
+        });
+      },
+      edit: function(req, res) {
+        Post.findById(req.params.id).then(post => {
+          res.render("post/edit", { post });
+        });
+      },
+      update: function(req, res) {
+        console.log(req.body);
+        const { title, description, link } = req.body;
+    
+        Post.findOneAndUpdate(
+          req.params.id,
+          {
+            title,
+            description,
+            link
+          },
+          {
+            runValidators: true
+          }
+        )
+          .then(post => {
+            res.redirect(`/post/${post._id}`);
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      },
+      delete: function(req, res) {
+        Post.remove({ _id: req.params.id }).then(post => {
+          console.log(post)
+          res.redirect("/");
+        });
+      }
+    };
+    
