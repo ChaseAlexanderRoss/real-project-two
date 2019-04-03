08d93a9f19daba78c126cd6f157693efb92cf393
ChaseAlexanderRoss <chase.alexander.ross@gmail.com>
2019-04-03 09:20:21 -0400

    changes for Heroku

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,18 @@
-const mongoose = require('mongoose')
-if (process.env.NODE_ENV == "production") {
-    mongoose.connect(process.env.DB_URL, {useNewUrlParser: true}, function(err) {
-        if (err) { return console.log(err);}
-      })
+const mongoose = require('mongoose');
 
+if (process.env.NODE_ENV == 'production') {
+	mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }, function(err) {
+		if (err) {
+			return console.log(err);
+		}
+	});
 } else {
-    mongoose.connect("mongodb://localhost/post", {useNewUrlParser: true}, function(err) {
-        if (err) { return console.log(err);}
-      })
+	mongoose.connect('mongodb://localhost/post', { useNewUrlParser: true }, function(err) {
+		if (err) {
+			return console.log(err);
+		}
+	});
 }
-mongoose.connect('mongodb://localhost/post')
 
-mongoose.Promise = Promise
-module.exports = mongoose
+mongoose.Promise = Promise;
+module.exports = mongoose;
