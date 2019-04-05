56215b6ff4a658361fe1643b1a3ff6a6b7c6fe32
ChaseAlexanderRoss <chase.alexander.ross@gmail.com>
2019-04-05 13:28:32 -0400

    HF

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const express = require('express')
-const app = express()
-const hbs = require('hbs')
-const parser = require('body-parser')
-const methodOverride = require('method-override')
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+const parser = require('body-parser');
+const methodOverride = require('method-override');
 
-app.set('view engine', 'hbs')
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'hbs');
 app.use(parser.urlencoded({ extended: true }));
-app.use(methodOverride('_method'))
-app.use(require('./routes/index.js'))
+app.use(methodOverride('_method'));
+app.use(require('./routes/index.js'));
 
-app.listen(3000, () => console.log('listenting on port 3k'))
+app.listen(3000, () => console.log('listenting on port 3k'));
