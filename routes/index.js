bcfcd25854025ea5e605f1ae4ce78f4ccdb12584
ChaseAlexanderRoss <chase.alexander.ross@gmail.com>
2019-04-05 13:20:50 -0400

    fixing heroku

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
-const express = require('express')
-const router = express.Router()
-const { Post } = require('../models/index')
+const express = require('express');
+const router = express.Router();
+const { Post } = require('../models/index');
 
 router.get('/', function(req, res) {
-    Post.find({}).then(posts => {
-        res.render('index', { posts });
-    });
+	res.render('index', { posts });
 });
 
 router.use('/post', require('./post.js'));
